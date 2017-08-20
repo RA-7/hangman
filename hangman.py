c89c35c7e0534181f070475dd5582c2c9d259071
RA-7 <klanecek.niko@gmail.com>
2017-08-20 21:34:27 +0200

    added class Hangman

--- hangman.py
+++ hangman.py
@@ -1,49 +1,56 @@
+import os 
 import random
-import os
 import sys
 
 
-words = [
-    'pizza', 'word', 'python', 'test', 'yellow', 'down', 'chair', 'kitties']
+class Hangman:
+    def __init__(self, guess_count=10):
+        self.words = ['pizza', 'word', 'python', 'test', 'yellow', 'down', 
+                      'chair', 'kitties']
+        self.secret_word = random.choice(self.words)
+        self.blanks = ('_' * len(self.secret_word))
+        self.guess_count = guess_count
 
-secret_word = random.choice(words)
-secret_letters = list(secret_word)
-blanks = ('_' * len(secret_word))
-guess_count = 10
-correct_guesses = []
+    def print_blanks(self):
+        print('You have {} guesses remaining!'.format(self.guess_count))
+        print(self.secret_word)
+        print()
+        print(self.blanks)
+        print()
 
-def print_blanks(guess_count):
-    print('You have {} guesses remaining!'.format(guess_count))
-    print()
-    print(blanks)
-    print()
+    def play(self):
+        os.system('clear')
+        self.print_blanks()
+        secret_letters = list(self.secret_word)
+        correct_guesses = []
+
+        while self.guess_count > 0:
+            self.blanks = list(self.blanks)
+            guess = input('Guess a letter: ').lower()
+            # make sure the user input is a single letter
+            while not guess.isalpha() or len(guess) > 1:
+                print('Please input a single letter!')
+                guess = input('Guess a letter: ')
+            os.system('clear')
+            while (guess in secret_letters) and (guess not in correct_guesses):
+                # get the index of the guessed letter
+                guess_index = secret_letters.index(guess)
+                # replace the first instance of the guessed letter in the secret
+                # word with a dash, so the second instance etc. can be found
+                secret_letters[guess_index] = '-'
+                # add the guessed letter to the row of blanks at the same index
+                self.blanks[guess_index] = guess
+            correct_guesses += guess
+            self.guess_count -= 1
+            self.blanks = ''.join(self.blanks)
+            self.print_blanks()
+            if '_' not in self.blanks:
+                print('You win!')
+                sys.exit(0)
+
+        print('You lose!')
 
 
-os.system('clear')
-print_blanks(guess_count)
-
-while guess_count > 0:
-    blanks = list(blanks)
-    guess = input('Guess a letter: ').lower()
-    # make sure the user input is a single letter
-    while not guess.isalpha() or len(guess) > 1:
-        print('Please input a single letter!')
-        guess = input('Guess a letter: ')
-    os.system('clear')
-    while (guess in secret_letters) and (guess not in correct_guesses):
-        # get the index of the guessed letter
-        guess_index = secret_letters.index(guess)
-        # replace the first instance of the guessed letter in the secret
-        # word with a dash, so the second instance etc. can be found
-        secret_letters[guess_index] = '-'
-        # add the guessed letter to the row of blanks at the same index
-        blanks[guess_index] = guess
-    correct_guesses += guess
-    guess_count -= 1
-    blanks = ''.join(blanks)
-    print_blanks(guess_count)
-    if '_' not in blanks:
-        print('You win!')
-        sys.exit(0)
-
-print('You lose!')
+if __name__ == '__main__':
+    game = Hangman()
+    game.play()
